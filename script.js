ed0f7230f7c2c1bbd20c8f18beb31ed423a5f73d
Ronald Weigan <roanldweigan2@gmail.com>
2024-07-03 02:34:14 -0300

    Atualizado aviso restaurante fechado

--- script.js
+++ script.js
@@ -138,22 +138,22 @@ addressInput,addEventListener("input", function(event){
 // Finalizar pedido
 checkoutBtn.addEventListener("click", function(){
 
-    // const isOpen = checkRestaurantOpen();
-    // if(!isOpen){
-    //     Toastify({
-    //         text: "Ops o restaurante está fechado!",
-    //         duration: 3000,
-    //         close: true,
-    //         gravity: "top", // `top` or `bottom`
-    //         position: "right", // `left`, `center` or `right`
-    //         stopOnFocus: true, // Prevents dismissing of toast on hover
-    //         style: {
-    //           background: "#ef4444",
-    //         },
-    //     }).showToast();
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        Toastify({
+            text: "Ops o restaurante está fechado!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#ef4444",
+            },
+        }).showToast();
             
-    //     return;
-    // }
+        return;
+    }
 
 
     if(cart.length === 0) return;
